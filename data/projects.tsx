5a4e8518ac21f7eb3c4013e798f4c9a96d251f35
Max-vS <max@vonstorch.com>
2023-07-30 10:57:14 +0200

    more dummy projecrs

--- data/projects.tsx
+++ data/projects.tsx
@@ -1,22 +1,26 @@
 export const projectData = [
     {
-        title: "Project Alpha",
-        text: "This is a description of the Project Alpha. It involves some exciting work on AI.",
+        title: "Project Zeus",
+        text: "Designed and developed a machine learning pipeline that improved prediction accuracy by 20% for a Fortune 500 company. The pipeline included data preprocessing, feature selection, model training and prediction steps."
     },
     {
-        title: "Project Beta",
-        text: "This is a description of the Project Beta. It focuses on machine learning technologies.",
+        title: "Project Athena",
+        text: "Built a responsive and user-friendly e-commerce website using React and Redux. The site features product listing, shopping cart, checkout and user authentication functionalities."
     },
     {
-        title: "Project Gamma",
-        text: "This is a description of the Project Gamma. This project is all about data analysis.",
+        title: "Project Poseidon",
+        text: "Developed a mobile fitness app using React Native. The app includes features for tracking workouts, setting goals, and viewing progress over time. Integrated with Google Fit and Apple Health."
     },
     {
-        title: "Project Delta",
-        text: "This is a description of the Project Delta. It is about building scalable web services.",
+        title: "Project Artemis",
+        text: "Created a dynamic data visualization dashboard using D3.js and React. The dashboard provides users with interactive visualizations and insights from large datasets, improving decision-making processes."
     },
     {
-        title: "Sailsetters",
-        text: "Founding of our own Student Initiative."
+        title: "Project Hera",
+        text: "Developed a secure and transparent voting system based on Blockchain technology. This system ensures that votes are tamper-proof and verifiable while maintaining voter privacy."
+    },
+    {
+        title: "Project Apollo",
+        text: "Designed a home automation system using IoT devices and a Raspberry Pi. The system enables control of home appliances through a mobile application, improving energy efficiency and convenience."
     }
 ];
